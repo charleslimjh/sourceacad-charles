cb4bb3ef853192a8a0e7a9da4200fa23bd2b56a6
Source Academy User <Charles Lim>
2021-08-31 00:37:24 +0800

    Changes made from Source Academy

--- pascal.js
+++ pascal.js
@@ -4,4 +4,23 @@ function pascal(row, position) {
         : pascal(row - 1, position - 1) + pascal(row - 1, position);
 }
 
-);
+display(pascal(4, 3));
+
+/*
+RECURSIVE PROCESS
+
+p(4,3)
+p(4-1, 3-1) + p(3-1, 3)
+p(3,2) + p(3,3)
+p(3-1, 2-1) + p(3-1, 2) + p(3,3)
+p(2,1) + p(2,2) + p(3,3)
+p(2-1, 1-1) + p(2-1, 1) + p(2,2) + p(3,3)
+p(1, 0) + p(1,1) + p(2,2) + p(3,3)
+1 + p(1,1) + p(2,2) + p(3,3)
+1 + 1 + p(2,2) + p(3,3)
+2 + p(2,2) + p(3,3)
+2 + 1 + p(3,3)
+3 + p(3,3)
+3 + 1
+4
+*/
