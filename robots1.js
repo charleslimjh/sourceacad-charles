d3d3d51bc6744ba4a45b192f942104eb928b35a8
Source Academy User <Charles Lim>
2021-10-05 17:46:12 +0800

    Changes made from Source Academy

--- robots1.js
+++ robots1.js
@@ -1,24 +1,21 @@
 // Studio 10J
 // Charles, Debbie, Han Wen, Sean, Shan, Ethan, Jia Cheng, Yuan Zheng
 
-//Mission 1: Getting started
-//Part1 1: 
-// ev3_speak("Vimuth is the best");
+// Mission 1: Getting started
+// Part 1: 
+ev3_speak("Vimuth is the best");
 
-function position() {
-    display(ev3_motorGetPosition(ev3_motorB()));
-    display(ev3_motorGetPosition(ev3_motorC()));    
-}
+
 
 //Part 2:
-
 function move(len_in_cm) {
     const pos = 18.6;
     ev3_runToRelativePosition(ev3_motorB(), len_in_cm * pos, 150);
     ev3_runToRelativePosition(ev3_motorC(), len_in_cm * pos, 150);
-    ev3_pause((len_in_cm * pos * 1000) / 150);
+    ev3_pause((len_in_cm * math_abs(pos) * 1000) / 150);
 }
 
+// TEST
 // move(10);
 
 // Part 3:
@@ -30,8 +27,8 @@ function aboutturn(deg) {
     ev3_pause(pos * math_floor(math_abs(deg) / 90) * 1000 / 150);
 }
 
+// TEST
 // aboutturn(90);
-// aboutturn(-90);
 
 
 //Part 4:
@@ -39,23 +36,29 @@ function aboutturn(deg) {
 const actions = list(
                     move(10),
                     // anti-clockwise
-                    ev3_pause(10000),
+                    ev3_pause(200),
                     aboutturn(90),
-                    ev3_pause(10000),
+                    ev3_pause(200),
                     move(5),
                     // clockwise
-                    ev3_pause(10000),
+                    ev3_pause(200),
                     aboutturn(-90),
-                    ev3_pause(10000),
+                    ev3_pause(200),
                     move(15));
 
-function do_actions(lst) {
-    for_each(x=>x, lst);
-}
-
-do_actions(actions);
+// TEST
+// for_each(x=>x actions);
 
 //Mission 2: thing
+
+// Part 1:
+
+function 
+    ev3_ultrasonicSensorDistance(ev3_ultrasonicSensor()) <= 10
+        ? move(-30)
+        :
+
+
 
 
 //Mission 3: more things
